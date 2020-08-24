8b9feb6aca69a13e829b3ed61869942d4e715c8f
maruf <maruf.harsono@gmail.com>
2020-08-24 17:18:20 +0700

    InboxMail: added popup

---
+++ src/views/InboxMail/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    max-width="490"
+  >
+    <v-card>
+      <v-card-title class="headline">{{ $t('label.attention') }}</v-card-title>
+      <v-card-text>
+        <span>{{ $t('label.warning_letter') }} </span>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="green darken-1"
+          text
+          @click="goDashboard"
+        >
+          {{ $t('label.ok') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  created() {
+    this.dialog = true
+  },
+  methods: {
+    goDashboard() {
+      this.dialog = false
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
