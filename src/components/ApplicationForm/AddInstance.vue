2c6a94733fff915d4086dda1476223194d5b33cb
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-09-22 14:26:56 +0700

    [BUGFIX] pesan validasi sebelumnya masih muncul ketika klik Batal
    Semua form tidak di isi lalu muncul pesan validasi, kemudian klik batal lalu klik form fasyankes kembali, pesan validasi sebelumnya masih muncul

--- src/components/ApplicationForm/AddInstance.vue
+++ src/components/ApplicationForm/AddInstance.vue
@@ -77,9 +77,7 @@
               :placeholder="$t('label.input_faskes_location')"
               outlined
             />
-            <ValidationProvider
-                rules="required"
-            >
+            <ValidationProvider>
             <v-label class="title"><b>{{ $t('label.upload_fasyankes_letter_permit') }}</b></v-label>
             <br>
             <v-label><i class="text-small-second-step">({{ $t('label.max_file_title') }})</i></v-label>
@@ -239,6 +237,7 @@ export default {
     ])
   },
   async created() {
+    await this.setShowDialog()
     await this.$store.dispatch('faskesType/getListFaskesType')
   },
   methods: {
@@ -263,7 +262,38 @@ export default {
       }
     },
     hideDialog() {
+      this.resetDialog()
       EventBus.$emit('dialogHide', false)
+    },
+    resetDefaultValue() {
+      this.listQuery = {}
+      this.isSuccess = false
+      this.isFail = false
+      this.isSelecting = false
+      this.selectedFile = null
+      this.selectedFileName = ''
+      this.isUpload = false
+      this.uploadAlert = false
+      this.isFileValid = false
+      this.$refs.observer.reset()
+    },
+    resetDialog() {
+      this.resetDefaultValue()
+      this.listQuery.id_tipe_faskes = 1
+      this.listQuery.nama_atasan = '-'
+      this.listQuery.nama_faskes = '-'
+      this.listQuery.nomor_izin_sarana = '-'
+      this.listQuery.nomor_registrasi = '-'
+      this.listQuery.permit_file = '-'
+    },
+    async setShowDialog() {
+      this.resetDefaultValue()
+      this.listQuery.id_tipe_faskes = null
+      this.listQuery.nama_atasan = null
+      this.listQuery.nama_faskes = null
+      this.listQuery.nomor_izin_sarana = null
+      this.listQuery.nomor_registrasi = null
+      this.listQuery.permit_file = null
     },
     repeatDialog() {
       this.isSuccess = false
