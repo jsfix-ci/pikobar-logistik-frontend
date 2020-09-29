429be08341e61092c381d5434559f55e1e8a4dc0
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-09-29 15:34:59 +0700

    Halaman Daftar Surat Masuk

--- src/store/modules/letter/mutations.js
+++ src/store/modules/letter/mutations.js
@@ -8,6 +8,15 @@ export default {
   SET_TOTAL_DATA_OUTGOING_MAIL: (state, data) => {
     state.totalDataOutgoingMail = data
   },
+  SET_LIST_INCOMING_MAIL: (state, data) => {
+    state.listIncomingMail = data
+  },
+  SET_TOTAL_LIST_INCOMING_MAIL: (state, data) => {
+    state.totalListIncomingMail = data
+  },
+  SET_TOTAL_DATA_INCOMING_MAIL: (state, data) => {
+    state.totalDataIncomingMail = data
+  },
   SET_APPLICATION_LETTER: (state, data) => {
     state.applicationLetter = data
   },
@@ -21,6 +30,12 @@ export default {
     state.totalListLetterApplication = data
   },
   SET_TOTAL_DATA_OUTGOING_MAIL_APPLICATION: (state, data) => {
+    state.totalDataLetterApplication = data
+  },
+  SET_TOTAL_LIST_INCOMING_MAIL_APPLICATION: (state, data) => {
+    state.totalListLetterApplication = data
+  },
+  SET_TOTAL_DATA_INCOMING_MAIL_APPLICATION: (state, data) => {
     state.totalDataLetterApplication = data
   },
   SET_DETAIL_LETTER_PRINT: (state, data) => {
