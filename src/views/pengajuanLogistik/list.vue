a9074235bfc50358caba243c96da0d1fc2cabc60
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-10-08 13:56:08 +0700

    Debug Limit List Barang max 3 per halaman

--- src/views/pengajuanLogistik/list.vue
+++ src/views/pengajuanLogistik/list.vue
@@ -176,7 +176,7 @@ export default {
       ],
       listQuery: {
         page: 1,
-        limit: 10,
+        limit: 3,
         sort: '',
         city_code: '',
         verification_status: '',
