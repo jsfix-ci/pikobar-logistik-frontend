7c389ac29f7e55d776a6c33d40c0c2b00352c51f
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-10-09 11:30:32 +0700

    Menghilangkan rupiah / pengkondisian mata uang

--- src/views/pengajuanLogistik/detail.vue
+++ src/views/pengajuanLogistik/detail.vue
@@ -985,19 +985,19 @@ export default {
     back() {
       this.$router.go(-1)
     },
-    formatCurrency(value, prefix) {
+    formatCurrency(value) {
       if (value) {
         const number_string = value.toString()
         const split = number_string.split(',')
         const modulo = split[0].length % 3
-        let rupiah = split[0].substr(0, modulo)
+        let result = split[0].substr(0, modulo)
         const thousand = split[0].substr(modulo).match(/\d{3}/gi)
         if (thousand) {
           const separator = modulo ? '.' : ''
-          rupiah += separator + thousand.join('.')
+          result += separator + thousand.join('.')
         }
-        rupiah = split[1] !== undefined ? rupiah + ',' + split[1] : rupiah
-        return prefix === undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '')
+        result = split[1] !== undefined ? result + ',' + split[1] : result
+        return result
       } else {
         return '0'
       }
