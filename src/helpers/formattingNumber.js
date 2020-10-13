52b8cdd8959c57a001b8cf9a91258d9b9469dc85
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-10-13 09:49:51 +0700

    Formatting Number di Dashboard

--- src/helpers/formattingNumber.js
+++ src/helpers/formattingNumber.js
@@ -17,6 +17,24 @@ class FormattingNumber {
     const currency = (value / 1).toFixed(0).replace('.', ',')
     return currency.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
   }
+
+  formatCurrency(value) {
+    if (value) {
+      const number_string = value.toString()
+      const split = number_string.split(',')
+      const modulo = split[0].length % 3
+      let result = split[0].substr(0, modulo)
+      const thousand = split[0].substr(modulo).match(/\d{3}/gi)
+      if (thousand) {
+        const separator = modulo ? '.' : ''
+        result += separator + thousand.join('.')
+      }
+      result = split[1] !== undefined ? result + ',' + split[1] : result
+      return result
+    } else {
+      return '0'
+    }
+  }
 }
 
 export { FormattingNumber as default }
