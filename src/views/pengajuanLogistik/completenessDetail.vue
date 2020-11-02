2810bba321f9153dc27a65b9e3f20314cb1dccb4
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2020-11-02 19:08:36 +0700

    Menambah Kolom Kelengkapan di daftar Permohonan
    Detail
    =============
    - Header pada list (Kelengkapan), isinya: Lengkap/ Belum Lengkap
    - Pada list ditampilkan keterangan "belum lengkap"
    - Muncul keterangan belum lengkap jika terdapat nama pemohon, instansi pemohon, alamat pemohon, No. HP Pemohon, No. HP Pemohon 2, Dokumen surat dan KTP jika belum lengkap
    - Keterangan belum lengkap dapat diklik dan diarahkan pada popup yang berisi poin-poin yang belum lengkap. Contoh redaksi:
    Permohonan belum lengkap
      - Nama Pemohon (Belum Ada)
      - Instansi pemohon (Belum Ada)
      - Alamat lengkap (Belum Ada)
      - No. HP Pemohon (Belum Ada)
      - No. HP Pemohon 2, (Belum Ada)
      - Dokumen surat (Belum Ada)
      - Dokumen tanda pengenal (Belum Ada)
    - Jika Permohonan sudah lengkap tidak perlu ditampilkan popup
    
    [v1.1.12]

---
+++ src/views/pengajuanLogistik/completenessDetail.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-dialog
+    v-model="show"
+    width="40%"
+    :persistent="true"
+  >
+    <v-card>
+      <div class="col-sm-12">
+        <v-card-text class="headerSection">
+          <span><h4><b>{{ $t('label.completeness_header') }}</b></h4></span>
+        </v-card-text>
+      </div>
+      <div v-if="data" class="col-sm-12 contentDialog">
+        <v-card-text>
+          <span>{{ $t('label.not_complete_description') + data.id + ':' }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.applicant.applicant_name" class="contentMargin">
+          <span>{{ $t('label.not_complete_applicant_name') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.agency_name" class="contentMargin">
+          <span>{{ $t('label.not_complete_agency_name') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.location_address" class="contentMargin">
+          <span>{{ $t('label.not_complete_agency_address') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.applicant.primary_phone_number" class="contentMargin">
+          <span>{{ $t('label.not_complete_applicant_primary_phone_number') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.applicant.secondary_phone_number" class="contentMargin">
+          <span>{{ $t('label.not_complete_applicant_secondary_phone_number') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.applicant.letter" class="contentMargin">
+          <span>{{ $t('label.not_complete_applicant_letter') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <v-card-text v-if="!data.applicant.file" class="contentMargin">
+          <span>{{ $t('label.not_complete_applicant_file') }}</span>
+          <span class="red--text">{{ $t('label.completeness_fail') }}</span>
+        </v-card-text>
+        <hr>
+      </div>
+      <v-col class="d-flex justify-center">
+        <v-btn
+          small
+          width="180px"
+          height="40px"
+          class="margin-btn-update-logistic-needs"
+          @click="closeDialog"
+        >
+          {{ $t('label.back') }}
+        </v-btn>
+      </v-col>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import EventBus from '@/utils/eventBus'
+
+export default {
+  name: 'CompletenessDetail',
+  props: {
+    show: {
+      type: Boolean,
+      default: null
+    }
+  },
+  data() {
+    return {
+      data: {}
+    }
+  },
+  methods: {
+    // Default Method
+    setData(id, value) {
+      this.data = value
+    },
+    closeDialog() {
+      EventBus.$emit('hideCompletenessDetail', false)
+    }
+  }
+}
+</script>
+
+<style>
+  .contentDialog {
+    margin-top: -50px;
+  }
+  .contentMargin {
+    margin-top: -20px;
+  }
+</style>
