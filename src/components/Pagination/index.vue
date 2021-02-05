508e8f684431767693fd8b8a63a2b7e5fe6e78f6
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-02-05 17:22:55 +0700

    Pagination Component - Total Data Dikirim ke komponen

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -1,25 +1,43 @@
 <template>
-  <v-row v-if="total > 0" class="pb-6">
+  <v-row v-if="total > 0" class="pb-6 mt-1">
     <v-col>
-      <v-select
-        v-model="pageSize"
-        :items="pageSizes"
-        filled
-        solo
-        dense
-        label="Baris"
-        class="col-md-3 col-sm-4 mt-1"
-        @change="changePageSize"
-      />
+      <v-row>
+        <v-col cols="6">
+          <v-text-field
+            :value="'Total Data: ' + totalData"
+            outlined
+            filled
+            solo
+            dense
+            readonly
+          />
+        </v-col>
+        <v-col cols="4">
+          <v-select
+            v-model="pageSize"
+            :items="pageSizes"
+            outlined
+            filled
+            solo
+            dense
+            label="Baris"
+            @change="changePageSize"
+          />
+        </v-col>
+      </v-row>
     </v-col>
-    <v-col cols="8">
-      <v-pagination
-        v-model="currentPage"
-        :length="total"
-        :total-visible="6"
-        class="float-right mr-n2"
-        @input="onNext"
-      />
+    <v-col cols="8" class="mr-n2">
+      <v-row>
+        <v-col>
+          <v-pagination
+            v-model="currentPage"
+            :length="total"
+            :total-visible="6"
+            class="float-right"
+            @input="onNext"
+          />
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
@@ -30,6 +48,9 @@ export default {
   props: {
     total: {
       required: true,
+      type: Number
+    },
+    totalData: {
       type: Number
     },
     page: {
