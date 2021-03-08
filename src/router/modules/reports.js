24dc3a30d8c3e2fe6aa740743a741cadca489bab
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-03-09 06:53:40 +0700

    Pelaporan Penerimaan Barang - Revisi Halaman On Progress

--- src/router/modules/reports.js
+++ src/router/modules/reports.js
@@ -13,7 +13,7 @@ const reports = {
   children: [
     {
       path: '/reports/goods_receipt_report',
-      component: () => import('@/views/exception/underMaintenance'),
+      component: () => import('@/views/reports/goods_receipt_report/list'),
       name: 'goods_receipt_report',
       meta: {
         title: 'goods_receipt_report',
@@ -23,7 +23,7 @@ const reports = {
     },
     {
       path: '/reports/goods_use_report',
-      component: () => import('@/views/exception/underMaintenance'),
+      component: () => import('@/views/reports/goods_use_report/list'),
       name: 'goods_use_report',
       meta: {
         title: 'goods_use_report',
--- src/views/reports/goods_receipt_report/list.vue
+++ src/views/reports/goods_receipt_report/list.vue
@@ -1,14 +1,25 @@
 <template>
-  <v-card class="main-card-landing-page" outlined>
-    <div class="text-card-main">
-      <v-row>
-        <img src="../../../static/maintenance.jpg" width="100%">
-      </v-row>
-    </div>
-  </v-card>
+  <div>
+    <v-card outlined>
+      <div>
+        <v-row class="mt-5 ml-5 mr-5 mb-5">
+          <v-col>
+            <img src="../../../static/logistic_acceptance_report.png" width="90%">
+          </v-col>
+          <v-col class="mt-10">
+            <div class="font-product-sans-landing-page card-feature-landing-page mb-5 font-weight-bold text-center">
+              {{ $t('label.logistic_acceptance_report_title') }}
+            </div>
+            <p class="font-text card-text-landing-page text-center">{{ $t('label.logistic_acceptance_report_description') }}</p>
+            <p class="font-text card-text-landing-page text-center">{{ $t('label.goods_receipt_report_description') }}</p>
+          </v-col>
+        </v-row>
+      </div>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
-  name: 'LandingPage'
+  name: 'GoodsReceiptReportList'
 }
 </script>
