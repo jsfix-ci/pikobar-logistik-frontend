98c7e22ff6c4f22869b3096f758d74a14ac464f3
maruf <maruf.harsono@gmail.com>
2021-09-06 15:59:25 +0700

    vaccineRequestForm: added conditional form for vaccine and alkes

--- src/components/ApplicationForm/ThirdStep.vue
+++ src/components/ApplicationForm/ThirdStep.vue
@@ -1,211 +1,195 @@
 <template>
   <v-container>
-    <div
-      v-if="!isAddAPD"
-    >
-      <center>
-        <v-img
-          src="../../static/apd.svg"
-          :aspect-ratio="1"
-          max-width="250px"
-          max-height="250px"
-        />
-      </center>
-      <v-col md="6" offset-md="3">
-        <center><v-label>{{ $t('label.add_logistics_label') }}</v-label></center>
-      </v-col>
-      <center>
-        <v-btn
-          color="#2E7D32"
-          outlined
-          @click="onClick"
-        >
-          {{ $t('label.add') }}
-        </v-btn>
-      </center>
-      <v-col cols="12" sm="12" md="4" offset-md="4">
-        <v-alert
-          v-if="showAlert"
-          type="error"
-        >
-          {{ $t('label.alert_logistic_needs') }}
-        </v-alert>
-      </v-col>
-    </div>
-    <ValidationObserver ref="observer">
-      <v-form
-        v-if="isAddAPD"
-        ref="form"
-        lazy-validation
-      >
-        <v-row
-          v-for="(data, index) in logisticNeeds"
-          :key="data.id"
-        >
-          <v-col
-            cols="12"
-            sm="12"
-            md="1"
+    <div v-if="!isAddAPD">
+      <third-step-options @next="onClick" />
+      <v-row>
+        <v-col cols="12" sm="12" md="4" offset-md="4">
+          <v-alert
+            v-if="showAlert"
+            type="error"
           >
-            <center><v-label class="title"><b>{{ index + 1 }}</b></v-label></center>
-            <center><v-icon class="padding-10-third-step" color="red" size="25" @click="deleteData(index)">mdi-delete</v-icon></center>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="12"
-            md="3"
-          >
-            <ValidationProvider
-              v-slot="{ errors }"
-              rules="requiredAPDName"
-            >
-              <v-label class="title"><b>{{ $t('label.apd_name_spec') }}</b></v-label>
-              <v-autocomplete
-                v-model="data.apd"
-                :placeholder="$t('label.choose_apd')"
-                :items="listAPD"
-                item-text="name"
-                item-value="id"
-                :error-messages="errors"
-                outlined
-                solo-inverted
-                @change="setUnit(data)"
-              />
-            </ValidationProvider>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="12"
-            md="2"
-          >
-            <ValidationProvider
-              v-slot="{ errors }"
-              rules="requiredBrand"
-            >
-              <v-label class="title"><b>{{ $t('label.description') }}</b></v-label>
-              <v-text-field
-                v-model="data.brand"
-                :placeholder="$t('label.input_description')"
-                :error-messages="errors"
-                outlined
-                solo-inverted
-              />
-            </ValidationProvider>
-          </v-col>
-          <v-col
-            cols="4"
-            sm="4"
-            md="2"
-          >
-            <ValidationProvider
-              v-slot="{ errors }"
-              rules="requiredTotal|notMinus|notDecimal"
-            >
-              <v-label class="title"><b>{{ $t('label.total') }}</b></v-label>
-              <v-text-field
-                v-model="data.total"
-                outlined
-                solo-inverted
-                type="number"
-                :error-messages="errors"
-                @change="setTotalAPD"
-              />
-            </ValidationProvider>
-          </v-col>
-          <v-col
-            cols="8"
-            sm="8"
-            md="2"
-          >
-            <ValidationProvider
-              v-slot="{ errors }"
-              rules="requiredUnit"
-            >
-              <v-label class="title"><b>{{ $t('label.unit') }}</b></v-label>
-              <v-autocomplete
-                v-model="data.unitId"
-                :items="data.unitList"
-                outlined
-                solo-inverted
-                :error-messages="errors"
-                item-value="unit_id"
-                item-text="unit"
-              />
-            </ValidationProvider>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="12"
-            md="2"
-          >
-            <ValidationProvider
-              v-slot="{ errors }"
-              rules="requiredPurpose"
-            >
-              <v-label class="title"><b>{{ $t('label.purpose') }}</b></v-label>
-              <v-text-field
-                v-model="data.purpose"
-                :placeholder="$t('label.input_purpose')"
-                outlined
-                solo-inverted
-                :error-messages="errors"
-              />
-            </ValidationProvider>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" sm="12" md="3" offset-md="1">
-            <v-label>{{ $t('label.logistic_total') }} {{ totalLogistic }}</v-label>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" sm="12" md="2" offset-md="1">
-            <center>
-              <v-btn
-                color="#2E7D32"
-                outlined
-                @click="addLogistic"
-              >{{ $t('label.add_more') }}</v-btn>
-            </center>
-          </v-col>
-          <v-col cols="12" sm="12" md="3" offset-md="1">
-            <v-alert
-              v-if="showAlert"
-              type="error"
-            >
-              {{ $t('label.alert_logistic_needs') }}
-            </v-alert>
-          </v-col>
-        </v-row>
-      </v-form>
-      <v-container fluid>
-        <v-col cols="6" sm="6" md="6" class="float-right-third-step">
-          <v-btn
-            class="btn-margin-positive"
-            color="primary"
-            @click="onNext"
-          >{{ $t('label.next') }}</v-btn>
-          <v-btn
-            class="btn-margin-positive"
-            outlined
-            text
-            @click="onPrev"
-          >{{ $t('label.cancel') }}</v-btn>
+            {{ $t('label.alert_logistic_needs') }}
+          </v-alert>
         </v-col>
-      </v-container>
-    </ValidationObserver>
+      </v-row>
+    </div>
+    <div v-else>
+      <ValidationObserver ref="observer">
+        <v-form
+          ref="form"
+          lazy-validation
+        >
+          <v-row
+            v-for="(data, index) in logisticNeeds"
+            :key="data.id"
+          >
+            <v-col
+              cols="12"
+              sm="12"
+              md="1"
+            >
+              <center><v-label class="title"><b>{{ index + 1 }}</b></v-label></center>
+              <center><v-icon class="padding-10-third-step" color="red" size="25" @click="deleteData(index)">mdi-delete</v-icon></center>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="12"
+              md="3"
+            >
+              <ValidationProvider
+                v-slot="{ errors }"
+                rules="requiredAPDName"
+              >
+                <v-label class="title"><b>{{ $t('label.apd_name_spec') }}</b></v-label>
+                <v-autocomplete
+                  v-model="data.apd"
+                  :placeholder="$t('label.choose_apd')"
+                  :items="listAPD"
+                  item-text="name"
+                  item-value="id"
+                  :error-messages="errors"
+                  outlined
+                  solo-inverted
+                  @change="setUnit(data)"
+                />
+              </ValidationProvider>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="12"
+              md="2"
+            >
+              <ValidationProvider
+                v-slot="{ errors }"
+                rules="requiredBrand"
+              >
+                <v-label class="title"><b>{{ $t('label.description') }}</b></v-label>
+                <v-text-field
+                  v-model="data.brand"
+                  :placeholder="$t('label.input_description')"
+                  :error-messages="errors"
+                  outlined
+                  solo-inverted
+                />
+              </ValidationProvider>
+            </v-col>
+            <v-col
+              cols="4"
+              sm="4"
+              md="2"
+            >
+              <ValidationProvider
+                v-slot="{ errors }"
+                rules="requiredTotal|notMinus|notDecimal"
+              >
+                <v-label class="title"><b>{{ $t('label.total') }}</b></v-label>
+                <v-text-field
+                  v-model="data.total"
+                  outlined
+                  solo-inverted
+                  type="number"
+                  :error-messages="errors"
+                  @change="setTotalAPD"
+                />
+              </ValidationProvider>
+            </v-col>
+            <v-col
+              cols="8"
+              sm="8"
+              md="2"
+            >
+              <ValidationProvider
+                v-slot="{ errors }"
+                rules="requiredUnit"
+              >
+                <v-label class="title"><b>{{ $t('label.unit') }}</b></v-label>
+                <v-autocomplete
+                  v-model="data.unitId"
+                  :items="data.unitList"
+                  outlined
+                  solo-inverted
+                  :error-messages="errors"
+                  item-value="unit_id"
+                  item-text="unit"
+                />
+              </ValidationProvider>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="12"
+              md="2"
+            >
+              <ValidationProvider
+                v-slot="{ errors }"
+                rules="requiredPurpose"
+              >
+                <v-label class="title"><b>{{ $t('label.purpose') }}</b></v-label>
+                <v-text-field
+                  v-model="data.purpose"
+                  :placeholder="$t('label.input_purpose')"
+                  outlined
+                  solo-inverted
+                  :error-messages="errors"
+                />
+              </ValidationProvider>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="12" md="3" offset-md="1">
+              <v-label>{{ $t('label.logistic_total') }} {{ totalLogistic }}</v-label>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="12" md="2" offset-md="1">
+              <center>
+                <v-btn
+                  color="#2E7D32"
+                  outlined
+                  @click="addLogistic"
+                >{{ $t('label.add_more') }}</v-btn>
+              </center>
+            </v-col>
+            <v-col cols="12" sm="12" md="3" offset-md="1">
+              <v-alert
+                v-if="showAlert"
+                type="error"
+              >
+                {{ $t('label.alert_logistic_needs') }}
+              </v-alert>
+            </v-col>
+          </v-row>
+        </v-form>
+        <v-container fluid>
+          <v-col cols="6" sm="6" md="6" class="float-right-third-step">
+            <v-btn
+              class="btn-margin-positive"
+              color="primary"
+              @click="onNext"
+            >{{ $t('label.next') }}</v-btn>
+            <v-btn
+              class="btn-margin-positive"
+              outlined
+              text
+              @click="onPrev"
+            >{{ $t('label.cancel') }}</v-btn>
+          </v-col>
+        </v-container>
+      </ValidationObserver>
+    </div>
   </v-container>
 </template>
 <script>
 import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import EventBus from '@/utils/eventBus'
 import { mapGetters } from 'vuex'
+import ThirdStepOptions from './ThirdStepOptions.vue'
 
 export default {
   name: 'KebutuhanLogistik',
   components: {
     ValidationProvider,
-    ValidationObserver
+    ValidationObserver,
+    ThirdStepOptions
   },
   props: {
     logisticNeeds: {
@@ -230,7 +214,7 @@ export default {
   },
   computed: {
     ...mapGetters('logistics', [
-      'listAPD', 'listApdUnit'
+      'listAPD', 'listApdUnit', 'logisticRequestType'
     ])
   },
   async created() {
---
+++ src/components/ApplicationForm/ThirdStepOptions.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <v-row>
+      <v-col md="5" offset-md="1">
+        <div class="d-flex justify-center">
+          <v-img
+            src="../../static/apd.svg"
+            :aspect-ratio="1"
+            max-width="250px"
+            max-height="250px"
+          />
+        </div>
+        <p class="text-center">Tambahkan Kebutuhan Alat Kesehatan</p>
+        <div class="d-flex justify-center">
+          <v-btn
+            color="#2E7D32"
+            outlined
+            @click="onClick('alkes')"
+          >
+            {{ $t('label.add') }}
+          </v-btn>
+        </div>
+      </v-col>
+      <v-col md="5">
+        <div class="d-flex justify-center">
+          <v-img
+            src="../../static/apd.svg"
+            :aspect-ratio="1"
+            max-width="250px"
+            max-height="250px"
+          />
+        </div>
+        <!-- <p class="text-center">{{ $t('label.add_logistics_label') }}</p> -->
+        <p class="text-center">Tambahkan Kebutuhan Vaksin</p>
+        <div class="d-flex justify-center">
+          <v-btn
+            color="#2E7D32"
+            outlined
+            @click="onClick('vaccine')"
+          >
+            {{ $t('label.add') }}
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ThirdStepOptions',
+  methods: {
+    onClick(type) {
+      this.$store.dispatch('logistics/logisticRequestType', type)
+      this.$emit('next')
+    }
+  }
+}
+</script>
