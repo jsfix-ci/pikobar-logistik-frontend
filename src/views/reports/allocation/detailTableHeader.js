240a82cf56ad4f3e0a9d1b120ae60d7e6ad2ab7f
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-09-08 20:22:48 +0700

    Create detailTableHeader.js

---
+++ src/views/reports/allocation/detailTableHeader.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    id: 'first-row',
+    data: [
+      {
+        label: 'No.',
+        rowSpan: 3
+      },
+      {
+        label: 'Nama Rumah Sakit/Puskesmas',
+        rowSpan: 3
+      },
+      {
+        label: 'Kabupaten/Kota',
+        rowSpan: 3
+      },
+      {
+        label: 'Alamat',
+        rowSpan: 3
+      },
+      {
+        label: 'JUMLAH TT RUANG ISOLASI COVID',
+        colSpan: 3
+      },
+      {
+        label: 'Non BTT',
+        colSpan: 4
+      },
+      {
+        label: 'BTT',
+        colSpan: 4
+      }
+    ]
+  },
+  {
+    id: 'second-row',
+    data: [
+      {
+        label: 'Hijau',
+        rowSpan: 2
+      },
+      {
+        label: 'Kuning',
+        rowSpan: 2
+      },
+      {
+        label: 'Merah',
+        rowSpan: 2
+      },
+      {
+        label: 'ALLPLEX PCR REAGENT (DBHCHT)'
+      },
+      {
+        label: 'MBIOCOV-19 RT-PCR KIT @100 (BNPB)'
+      },
+      {
+        label: ''
+      }
+    ]
+  }
+]
