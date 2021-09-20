81804e75b4ac86b0890223859ba0dfab5cdddb63
maruf <maruf.harsono@gmail.com>
2021-09-20 09:37:00 +0700

    refactorConfirmtionPage: added component loading

---
+++ src/components/ApplicationForm/confirmation/loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+      <img height="200" src="@/static/mengirim.svg">
+    </v-row>
+    <v-row align="center" justify="center" class="margin-wait-data-confirmation">
+      {{ $t('label.wait') }}
+    </v-row>
+    <v-row align="center" justify="center" class="mt-5">
+      {{ $t('label.loading_step_one') }}
+    </v-row>
+    <v-row align="center" justify="center">
+      {{ $t('label.loading_step_two') }}
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoadingPage'
+}
+</script>
