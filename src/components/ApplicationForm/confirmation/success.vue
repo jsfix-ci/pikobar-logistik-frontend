966a8b67692871f84c60e62149dab8f86848a469
maruf <maruf.harsono@gmail.com>
2021-09-20 09:37:49 +0700

    refactorConfirmationPage: added success page component

---
+++ src/components/ApplicationForm/confirmation/success.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-row align="center" justify="center">
+      <img height="200" src="@/static/berhasil.svg">
+    </v-row>
+    <v-row align="center" justify="center">
+      <div class="save-style-data-confirmation">{{ $t('label.save_success') }}</div>
+    </v-row>
+    <v-row align="center" justify="center" class="confirmation-text mt-7">
+      {{ $t('label.loading_done_one') }}
+    </v-row>
+    <v-row align="center" justify="center" class="confirmation-text mt-3">
+      {{ $t('label.loading_done_two') }}
+    </v-row>
+    <br>
+    <v-row align="center" justify="center" class="mt-3 mb-15">
+      <v-btn href="/#/landing-page">{{ $t('label.back') }}</v-btn>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SuccessRequest'
+}
+</script>
