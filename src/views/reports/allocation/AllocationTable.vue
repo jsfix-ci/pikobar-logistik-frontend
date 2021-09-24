53f3ed49fbcc327c7267809b6cb4203dda04aab2
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-09-24 15:01:43 +0700

    remove div in AllocationTable component

--- src/views/reports/allocation/AllocationTable.vue
+++ src/views/reports/allocation/AllocationTable.vue
@@ -1,62 +1,60 @@
 <template>
-  <div>
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th
-              v-for="header in tableHeader"
-              :key="header.label"
-              :class="header.class"
-            >
-              {{ header.label }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-if="listData.length === 0">
-            <td
-              colspan="7"
-              class="text-center"
-            >
-              {{ $t('label.no_data') }}
-            </td>
-          </tr>
-          <tr
-            v-for="(data, index) in listData"
-            v-else
-            :key="data.id"
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th
+            v-for="header in tableHeader"
+            :key="header.label"
+            :class="header.class"
           >
-            <td>{{ getTableRowNumbering(index) }}</td>
-            <td>{{ data.letter_number }}</td>
-            <td>{{ data.allocation_distribution_requests_count }}</td>
-            <td>{{ data.allocation_material_requests_total }}</td>
-            <td>{{ $moment(data.letter_date).format('D MMMM YYYY') }}</td>
-            <td>
-              <span
-                :class="{
-                  'status__draft': data.status === 'draft',
-                  'status__success': data.status === 'success'
-                }"
-              >
-                {{ capitalize(data.status) }}
-              </span>
-            </td>
-            <td class="text-center">
-              <v-btn
-                text
-                small
-                color="info"
-                @click="toDetail(data)"
-              >
-                {{ $t('label.detail') }}
-              </v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </div>
+            {{ header.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="listData.length === 0">
+          <td
+            colspan="7"
+            class="text-center"
+          >
+            {{ $t('label.no_data') }}
+          </td>
+        </tr>
+        <tr
+          v-for="(data, index) in listData"
+          v-else
+          :key="data.id"
+        >
+          <td>{{ getTableRowNumbering(index) }}</td>
+          <td>{{ data.letter_number }}</td>
+          <td>{{ data.allocation_distribution_requests_count }}</td>
+          <td>{{ data.allocation_material_requests_total }}</td>
+          <td>{{ $moment(data.letter_date).format('D MMMM YYYY') }}</td>
+          <td>
+            <span
+              :class="{
+                'status__draft': data.status === 'draft',
+                'status__success': data.status === 'success'
+              }"
+            >
+              {{ capitalize(data.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <v-btn
+              text
+              small
+              color="info"
+              @click="toDetail(data)"
+            >
+              {{ $t('label.detail') }}
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
