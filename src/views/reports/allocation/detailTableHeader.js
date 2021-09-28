80d554de987b0eca824eb149cf1ac8365fed681c
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-09-28 13:11:41 +0700

    Update detailTableHeader.js

--- src/views/reports/allocation/detailTableHeader.js
+++ src/views/reports/allocation/detailTableHeader.js
@@ -1,61 +1,23 @@
+import i18n from '@/lang'
 export default [
   {
-    id: 'first-row',
-    data: [
-      {
-        label: 'No.',
-        rowSpan: 3
-      },
-      {
-        label: 'Nama Rumah Sakit/Puskesmas',
-        rowSpan: 3
-      },
-      {
-        label: 'Kabupaten/Kota',
-        rowSpan: 3
-      },
-      {
-        label: 'Alamat',
-        rowSpan: 3
-      },
-      {
-        label: 'JUMLAH TT RUANG ISOLASI COVID',
-        colSpan: 3
-      },
-      {
-        label: 'Non BTT',
-        colSpan: 4
-      },
-      {
-        label: 'BTT',
-        colSpan: 4
-      }
-    ]
+    id: 1,
+    label: i18n.t('label.agency_name')
   },
   {
-    id: 'second-row',
-    data: [
-      {
-        label: 'Hijau',
-        rowSpan: 2
-      },
-      {
-        label: 'Kuning',
-        rowSpan: 2
-      },
-      {
-        label: 'Merah',
-        rowSpan: 2
-      },
-      {
-        label: 'ALLPLEX PCR REAGENT (DBHCHT)'
-      },
-      {
-        label: 'MBIOCOV-19 RT-PCR KIT @100 (BNPB)'
-      },
-      {
-        label: ''
-      }
-    ]
+    id: 2,
+    label: i18n.t('label.goods_item')
+  },
+  {
+    id: 3,
+    label: i18n.t('label.item_count')
+  },
+  {
+    id: 4,
+    label: i18n.t('label.item_unit')
+  },
+  {
+    id: 5,
+    label: i18n.t('label.purpose')
   }
 ]
