742e2d523de5c6108ee2a08cea6ce008faa9a653
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-10-05 18:41:16 +0700

    update table header content

--- src/views/reports/allocation/detailTableHeader.js
+++ src/views/reports/allocation/detailTableHeader.js
@@ -1,23 +1,9 @@
 import i18n from '@/lang'
 export default [
   {
-    id: 1,
+    label: i18n.t('label.number')
+  },
+  {
     label: i18n.t('label.agency_name')
-  },
-  {
-    id: 2,
-    label: i18n.t('label.goods_item')
-  },
-  {
-    id: 3,
-    label: i18n.t('label.item_count')
-  },
-  {
-    id: 4,
-    label: i18n.t('label.item_unit')
-  },
-  {
-    id: 5,
-    label: i18n.t('label.purpose')
   }
 ]
