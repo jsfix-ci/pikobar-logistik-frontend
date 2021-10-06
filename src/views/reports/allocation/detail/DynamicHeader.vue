0720ee89374e7704cbe38a1e3cb7d58370b8c304
Naufal Ihsan Kusumayadhi <56071566+naufalihsank@users.noreply.github.com>
2021-10-06 13:51:47 +0700

    create DynamicHeader component

---
+++ src/views/reports/allocation/detail/DynamicHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex flex-column">
+    <span class="content px-4">
+      {{ header.materialId }}
+    </span>
+    <span class="content px-4">
+      {{ header.label }}
+    </span>
+    <span class="px-4">
+      {{ $t('label.total_2') }} :
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  border-bottom: 1px solid white;
+}
+</style>
