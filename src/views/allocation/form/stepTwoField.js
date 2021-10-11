aa52ace4e7a6513bb03e350e7da3479146bf9795
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-10-11 16:02:12 +0700

    create static step two allocation form

---
+++ src/views/allocation/form/stepTwoField.js
@@ -0,0 +1,28 @@
+import i18n from '@/lang'
+export default [
+  {
+    label: i18n.t('label.instance_name'),
+    placeholder: i18n.t('label.choose_instance'),
+    options: 'instance'
+  },
+  {
+    label: i18n.t('label.goods_item'),
+    placeholder: i18n.t('label.choose_item'),
+    options: 'item'
+  },
+  {
+    label: i18n.t('label.item_count'),
+    placeholder: i18n.t('label.input_total'),
+    options: 'count'
+  },
+  {
+    label: i18n.t('label.item_unit'),
+    placeholder: i18n.t('label.choose_unit'),
+    options: 'unit'
+  },
+  {
+    label: i18n.t('label.purpose'),
+    placeholder: i18n.t('label.input_purpose'),
+    options: 'purpose'
+  }
+]
