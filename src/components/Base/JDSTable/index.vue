361da9a55af6dbbfa8511e91955a8ff9413c4bf5
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-03-09 18:06:55 +0700

    chore (base-table): create base jds table component

---
+++ src/components/Base/JDSTable/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :no-data-text="$t('label.no_data')"
+    hide-default-footer
+  >
+    <template v-slot:item="{ item, index }">
+      <slot name="item-prop" :item="item" :index="index" />
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      default: () => []
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-data-table::v-deep {
+  .v-data-table__wrapper {
+    border-radius: 8px;
+  }
+
+  .v-data-table-header {
+    background-color: #069550 !important;
+    color: white !important;
+  }
+
+  table {
+    border-radius: 8px;
+  }
+
+  tbody {
+    background-color: #FFFFFF !important;
+  }
+
+  th {
+    span {
+      color: white !important;
+      font-size: 14px;
+      font-family: 'Roboto', sans-serif;
+      font-weight: 700;
+    }
+  }
+
+  td {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px !important;
+  }
+}
+</style>
