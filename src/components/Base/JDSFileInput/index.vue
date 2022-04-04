fa80e3a9ae4fe1a64fbc756f9e54106b9971b11b
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-04-04 16:06:06 +0700

    feat (jds-file-input): create jds file input component

---
+++ src/components/Base/JDSFileInput/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="jds-file-input">
+    <span class="jds-file-input__label">
+      {{ label }}
+    </span>
+    <input
+      ref="file"
+      type="file"
+      class="d-none"
+      accept=".jpg,.jpeg,.png"
+      @input="onInput"
+      @change="onChange"
+    >
+    <div
+      class="jds-file-input__container"
+      @click="$refs.file.click()"
+      @dragover="dragover($event)"
+      @drop="drop($event)"
+    >
+      <img v-if="!showFileName" src="/img/file-input.svg" alt="file-input" height="72" width="93.35">
+      <span v-if="!showFileName" class="jds-file-input__placeholder">
+        Geser gambar/foto di sini atau
+        <span class="jds-file-input__placeholder jds-file-input__placeholder__upload" @click="$refs.file.click()">
+          Upload
+        </span>
+      </span>
+      <span v-else>{{ fileList[0].name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      fileList: []
+    }
+  },
+  computed: {
+    showFileName() {
+      return this.fileList.length !== 0
+    }
+  },
+  methods: {
+    onInput() {
+      this.$emit('input', this.$refs.file.files[0])
+    },
+    onChange() {
+      this.fileList = [...this.$refs.file.files]
+      this.$emit('change', this.$refs.file.files)
+    },
+    drop(event) {
+      event.preventDefault()
+      this.$refs.file.files = event.dataTransfer.files
+      this.onChange()
+      this.onInput()
+    },
+    dragover(event) {
+      event.preventDefault()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.jds-file-input {
+  display: flex;
+  flex-direction: column;
+
+  &__label {
+    font-family: 'Lato', sans-serif;
+    font-size: 15px;
+    color: #424242;
+    margin-bottom: 8px;
+  }
+
+  &__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #FAFAFA;
+    padding: 40px;
+    border-style: dashed;
+    border-width: 1px;
+    border-radius: 12px;
+    border-color: #9E9E9E;
+  }
+
+  &__placeholder {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    color: #424242;
+
+    &__upload {
+      color: #1e88e5;
+      text-decoration: underline;
+    }
+
+    &__upload:hover {
+      cursor: pointer;
+    }
+  }
+
+}
+</style>
