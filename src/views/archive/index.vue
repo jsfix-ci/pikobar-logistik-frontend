d0a285ff2a6eae21d2bdbdff89e87d86fedc5cdf
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-06-09 14:02:29 +0700

    fix (sentry): solving sentry issues

--- src/views/archive/index.vue
+++ src/views/archive/index.vue
@@ -123,6 +123,7 @@ export default {
         // { text: this.$t('label.action'), sortable: false }
       ],
       listQuery: {
+        page_type: 'archive',
         page: parseInt(this.$route.query?.page || 1),
         limit: parseInt(this.$route.query?.limit || 5),
         sort: this.$route.query?.sort || '',
