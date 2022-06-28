2db12be5225d8ce8d3ba8d4d4e1cd00e837e1635
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-06-28 10:27:35 +0700

    feat (jds-table): add jds sorting icon

--- src/components/Base/JDSTable/index.vue
+++ src/components/Base/JDSTable/index.vue
@@ -1,6 +1,5 @@
 <template>
   <v-data-table
-    :headers="headers"
     :items="items"
     :no-data-text="$t('label.no_data')"
     :loading="loading"
@@ -11,6 +10,31 @@
   >
     <template v-slot:item="{ item, index }">
       <slot name="item-prop" :item="item" :index="index" />
+    </template>
+
+    <template v-slot:header>
+      <thead>
+        <tr>
+          <th
+            v-for="item in headers"
+            :key="item.value"
+            class="v-data-table-header"
+            @click="item.sortable === undefined || item.sortable ? onSort(item.value) : ''"
+          >
+            <div class="d-flex flex-row justify-space-between align-center">
+              {{ item.text }}
+              <img
+                v-if="item.sortable === undefined || item.sortable"
+                width="16"
+                height="16"
+                alt="sort"
+                class="v-data-table-header__sort"
+                :src="sortIcon(item)"
+              >
+            </div>
+          </th>
+        </tr>
+      </thead>
     </template>
   </v-data-table>
 </template>
@@ -40,11 +64,28 @@ export default {
       options: {}
     }
   },
-  watch: {
-    options: {
-      deep: true,
-      handler(val) {
-        this.$emit('onSort', val)
+  methods: {
+    onSort(column) {
+      if (this.options.sortBy[0] === column) {
+        if (this.options.sortDesc[0]) {
+          this.options.sortDesc.splice(0, 1)
+          this.options.sortBy.splice(0, 1)
+        } else {
+          this.options.sortDesc[0] = true
+        }
+      } else {
+        this.options.sortBy = []
+        this.options.sortDesc = []
+        this.options.sortBy.push(column)
+        this.options.sortDesc.push(false)
+      }
+      this.$emit('onSort', this.options)
+    },
+    sortIcon(item) {
+      if (item.value === this.options.sortBy[0]) {
+        return this.options.sortDesc[0] ? '/img/icons/descending.svg' : '/img/icons/ascending.svg'
+      } else {
+        return '/img/icons/sortable.svg'
       }
     }
   }
@@ -60,6 +101,13 @@ export default {
   .v-data-table-header {
     background-color: #069550 !important;
     color: white !important;
+    font-size: 14px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 700;
+
+    &__sort:hover {
+      cursor: pointer;
+    }
   }
 
   table {
