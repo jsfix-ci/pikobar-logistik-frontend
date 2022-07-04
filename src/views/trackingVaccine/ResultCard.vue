fa9a1fd5b9510a8c187a760aea1fae4c81524ce4
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-07-04 16:09:44 +0700

    refactor (result-card): restructure col and row

--- src/views/trackingVaccine/ResultCard.vue
+++ src/views/trackingVaccine/ResultCard.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="result">
-    <div class="result__column result__column--header">
-      <span class="mb-2">No.</span>
-      <span class="mb-2">Tanggal Permohonan</span>
-      <span class="mb-2">Nomor Surat</span>
-      <span class="mb-2">Nama Instansi</span>
-      <span class="mb-2">Jenis Instansi</span>
-      <span class="mb-2">Pemohon</span>
-      <span>Aksi</span>
-    </div>
-    <div class="result__column">
-      <span class="mb-2">{{ index }}</span>
-      <span class="mb-2">{{ data.created_at }}</span>
-      <span class="mb-2">{{ data.letter_number }}</span>
-      <span class="mb-2">{{ data.agency_name }}</span>
-      <span class="mb-2">{{ data.agency_type_name }}</span>
-      <span class="mb-2">{{ data.applicant_fullname }}</span>
-      <JDSButton inverted height="34px" @click="onDetail()">
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">No.</v-col>
+      <v-col class="result__column" cols="6">{{ index }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Tanggal Permohonan</v-col>
+      <v-col class="result__column" cols="6">{{ data.created_at }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Nomor Surat</v-col>
+      <v-col class="result__column" cols="6">{{ data.letter_number }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Nama Instansi</v-col>
+      <v-col class="result__column" cols="6">{{ data.agency_name }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Jenis Instansi</v-col>
+      <v-col class="result__column" cols="6">{{ data.agency_type_name }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Pemohon</v-col>
+      <v-col class="result__column" cols="6">{{ data.applicant_fullname }}</v-col>
+    </v-row>
+    <v-row>
+      <v-col class="result__column result__column--header" cols="6">Aksi</v-col>
+      <v-col class="result__column" cols="6"><JDSButton inverted height="34px" @click="onDetail()">
         {{ $t('label.detail') }}
-      </JDSButton>
-    </div>
+      </JDSButton></v-col>
+    </v-row>
   </div>
 </template>
 
@@ -50,7 +60,7 @@ export default {
 <style lang="scss" scoped>
 .result {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding: 16px;
   background: #FFFFFF;
   font-family: 'Roboto', sans-serif;
