bc55661c12998fb27c29666235d7871ef708bd6e
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-08-09 11:36:23 +0700

    feat (cito): add 'Mendesak' in table list

--- src/views/logisticPost/deliveryPlan/index.vue
+++ src/views/logisticPost/deliveryPlan/index.vue
@@ -30,10 +30,10 @@
               <span
                 :class="{
                   'urgency': true,
-                  'urgency--red': item.is_urgency
+                  'urgency--red': item.is_urgency || item.is_cito
                 }"
               >
-                {{ item.is_urgency ? 'Segera' : 'Biasa' }}
+                {{ item.is_cito ? 'Mendesak' : item.is_urgency ? 'Segera' : 'Biasa' }}
               </span>
             </td>
             <td>
