45b85de5012dca174a35bab09e5ed865de494d05
jsfix.live analysis <info@coana.tech>
2022-11-19 07:44:19 +0100

    JSFIX patches

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -3,11 +3,11 @@ import Cookies from 'js-cookie'
 const TokenKey = 'Survey-Token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return JSON.parse(Cookies.get(TokenKey))
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, JSON.stringify(token))
 }
 
 export function removeToken() {
